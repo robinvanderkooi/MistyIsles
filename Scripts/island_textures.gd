aecf0a9414917f3f4f442ac01d9746aef97cc0a7
Robin <robinvanderkooi22@gmail.com>
2025-02-20 23:41:18 -0700

    Better island

--- Scripts/island_textures.gd
+++ Scripts/island_textures.gd
@@ -18,31 +18,44 @@ func _process(delta: float) -> void:
 func _initialize_result():
 	if not initialized_result:
 		var childs = self.get_children()
-		var resultname = %Result.name
-		var t0 :Texture2D = %Result.texture
-		var i0 :Image = t0.get_image()
-		var resetimage = true
-		var tex_count = 1
-		for chil in childs:
-			if resetimage :
-				resetimage = false
-				for x in i0.get_width():
-					for y in i0.get_height():
-						i0.set_pixel(x,y,Color.WHITE)
-			var t1:TextureRect = chil
-			if t1.name == resultname : continue
-			tex_count += 1
-			var tex :Texture = t1.texture
-			var i1 :Image = tex.get_image()
-			for x in i1.get_width():
-				for y in i1.get_height():
-					var p0 = i0.get_pixel(x,y)
-					var p1 = i1.get_pixel(x,y)
-					var px = (p0 * p1) * 1.2
-					i0.set_pixel(x,y, px)
+		
+		var img = Image.create(childs[0].texture.width,childs[0].texture.height,true,Image.FORMAT_RGBA8)
+		
+		var imgs = []
+		for chil:TextureRect in childs:
+			imgs.append(chil.texture.get_image())
+		for x in childs[0].texture.width:
+			for y in childs[0].texture.height:
+				var r = 1.0
+				var g = 1.0
+				var b = 1.0
+				for i in imgs.size():
+					var p : Color = imgs[i].get_pixel(x,y)
+					r *= p.r
+					g *= p.g
+					b *= p.b
+				#r /= float(childs.size())
+				#g /= float(childs.size())
+				#b /= float(childs.size())
+				img.set_pixel(x,y, Color(r,g,b,1.0))
+		var maxr = 0.0
+		for x in childs[0].texture.width:
+			for y in childs[0].texture.height:
+				var p = img.get_pixel(x,y)
+				if p.r > maxr : maxr = p.r
+		var sca = 1.0 / maxr
+		for x in childs[0].texture.width:
+			for y in childs[0].texture.height:
+				img.set_pixel(x,y, img.get_pixel(x,y) * sca)
+		
+		var itex : ImageTexture = ImageTexture.create_from_image(img)
+		#tex.texture = itex
+		#tex.size = Vector2i(64,64)
+		#%Textures.add_child(tex)
+		%Result.texture = itex
 		initialized_result = true
-		i0.save_png("res://result_image.png")
 		%Result.queue_redraw()
+		#img.save_png("res://result_image.png")
 	pass
 
 func _refresh_result():
